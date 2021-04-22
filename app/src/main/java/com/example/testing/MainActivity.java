2846933de7e708c07acbbfe989f0e428fed562fc
codesigood <41294530+codesigood@users.noreply.github.com>
2021-04-22 03:42:03 -0700

    Initial commit

---
+++ app/src/main/java/com/example/testing/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.testing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/testing/Student.java
@@ -0,0 +1,38 @@
+package com.example.testing;
+
+public class Student {
+    private String name, department;
+
+    public Student() {
+    }
+
+    public Student(String name, String department) {
+        this.name = name;
+        this.department = department;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", department='" + department + '\'' +
+                '}';
+    }
+}
